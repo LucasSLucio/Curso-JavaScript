8f4fa403ce070677645b0ebd2b65a4ea68529073
Lucas Silva <lucasdevcontato64@gmail.com>
2026-01-06 19:31:18 -0300

    fiz o desafio 003

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -4,18 +4,12 @@ do {
   c++;
 } while (c <= 2);
 
-/* var c = 1
+/* usando apenas o while
+ var c = 1
 while (c <= 10) {
     console.log(`Passo ${c}`)
     c++
     
 }
-    */
-
-/*console.log('Tudo bem?') 
-  console.log('Tudo bem?') 
-  console.log('Tudo bem?') 
-  console.log('Tudo bem?') 
-  console.log('Tudo bem?')
-  console.log('Tudo bem?')
+ 
   */
